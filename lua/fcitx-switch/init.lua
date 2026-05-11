ca300409e3756405c5e328287b9c039850dda196
moningf <themoningf@gmail.com>
2026-05-11 18:54:08 +0800

    init fcitx-switch.nvim

---
+++ lua/fcitx-switch/init.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+function M.to_english()
+  vim.system("fcitx5-remote", "-c")
+end
+
+-- 初始化函数
+function M.setup()
+  vim.api.nvim_create_autocmd("InsertLeave", {
+    callback = function()
+      M.to_english()
+    end
+  })
+end
+
+return M
