b961b82259012088b3a1c32b98d7ecc314003208
moningf <themoningf@gmail.com>
2026-05-11 19:15:01 +0800

    update

--- lua/fcitx-switch/init.lua
+++ lua/fcitx-switch/init.lua
@@ -4,12 +4,15 @@ function M.to_english()
   vim.system({ "fcitx5-remote", "-c" })
 end
 
--- 初始化函数
 function M.setup()
+  -- 检查是否存在
+  if vim.fn.executable("fcitx5-remote") == 0 then
+    return
+  end
+
+  -- 创建自动命令
   vim.api.nvim_create_autocmd("InsertLeave", {
-    callback = function()
-      M.to_english()
-    end
+    callback = M.to_english
   })
 end
 
